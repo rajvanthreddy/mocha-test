d886f16afb3ed08b0f65e5b28b9b11983dde048d
rajvanthreddy <rajvanth@gmail.com>
2020-05-04 11:46:12 -0400

    test

--- test/example.js
+++ test/example.js
@@ -3,6 +3,7 @@ const assert  = require('assert');
 
 const pluralize = require('../lib/example').pluralize;
 
+
 describe('example', function () {
   describe('pluralize', function () {
     it('keeps singular when count is 1', function () {
